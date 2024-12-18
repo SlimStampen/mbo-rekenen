49389abc72dc2fab9bf69841561eeeb866aecc7b
Maarten van der Velde <maarten@memorylab.nl>
2024-12-18 15:20:03 +0100

    Update Noorderpoort scores analysis

--- scripts/test_scores_noorderpoort.R
+++ scripts/test_scores_noorderpoort.R
@@ -8,31 +8,26 @@ source(theme_memorylab_url)
 
 
 # Pretest
-pretest <- read_excel(here("data", "test", "Noorderpoort Pretest Scores.xlsx")) |>
+pretest <- read_excel(here("data", "test", "noorderpoort_scores.xlsx"), sheet = "Nulmeting") |>
   setDT()
 
-# Remove non-data rows
-pretest <- pretest[4:.N]
+# Remove non-data row
+pretest <- pretest[1:.N-1]
 
-pretest_long <- melt(pretest, id.vars = "email", variable.name = "component", value.name = "score")
+pretest_long <- melt(pretest, id.vars = "Email", variable.name = "component", value.name = "score")
 pretest_long[, test := "Pretest"]
 pretest_long[, component := as.character(component)]
 
-pretest_long[component == "Aftreken & Optellen", component := "Aftrekken & Optellen"]
-
-
 # Posttest
-posttest <- read_excel(here("data", "test", "Noorderpoort Posttest Scores.xlsx")) |>
+posttest <- read_excel(here("data", "test", "noorderpoort_scores.xlsx"), sheet = "Posttest") |>
   setDT()
 
-# Remove non-data rows
-posttest <- posttest[4:.N]
+# Remove non-data row
+posttest <- posttest[1:.N-1]
 
-posttest_long <- melt(posttest, id.vars = "email", variable.name = "component", value.name = "score")
+posttest_long <- melt(posttest, id.vars = "Email", variable.name = "component", value.name = "score")
 posttest_long[, test := "Posttest"]
 posttest_long[, component := as.character(component)]
-
-posttest_long[component == "Vermenigvuldigingen", component := "Vermenigvuldigen"]
 
 # Combine
 tests <- rbind(pretest_long, posttest_long)
@@ -55,13 +50,17 @@ tests[, component := factor(component, levels = c("Delen",
 tests_avg <- tests[, .(mean_score = mean(score)), by = .(test, component)]
 tests_avg
 
+# Difference in average score per component between pretest and posttest (not paired!)
+tests_avg_change <- tests_avg[, .(change = diff(mean_score)), by = component]
+tests_avg_change[, change := round(change, 2)]
+tests_avg_change
+
 # Plot individual scores
 pos_jitter <- position_jitter(width = .05, height = .05, seed = 0)
 
 ggplot(tests, aes(x = test, y = score)) +
   facet_wrap(~ component, scales = "free_y") +
-  # geom_point(alpha = .2, position = pos_jitter) +
-  geom_line(aes(group = email), alpha = .2, position = pos_jitter) +
+  geom_line(aes(group = Email), alpha = .2, position = pos_jitter) +
   geom_point(data = tests_avg, aes(y = mean_score), color = colours_memorylab[4], size = 2) +
   geom_line(data = tests_avg, aes(y = mean_score, group = component), color = colours_memorylab[4], linewidth = 1) +
   labs(x = NULL, y = "Score", title = "Score per component (Noorderpoort)") +
